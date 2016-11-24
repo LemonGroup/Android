7341a291b34b206bd65ec91e89b9e5f5b17a39b3
GulevskiAA <gulevskiaa@gmail.com>
2016-11-24 08:38:20 +0200

    Added logout button in toolbar

---
+++ app/src/main/java/com/slack/geekbrainswork/ai/view/activities/MainView.java
@@ -0,0 +1,8 @@
+package com.slack.geekbrainswork.ai.view.activities;
+
+import com.slack.geekbrainswork.ai.view.View;
+
+public interface MainView extends View {
+
+    void navigateToLoginView();
+}
